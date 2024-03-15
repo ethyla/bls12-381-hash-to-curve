a958b6252f3023dbe0bae7c0612b0f990654196f
ethyla <ethyla@disroot.org>
2024-03-16 00:00:00 +0100

    Inital hash to field implementation

---
+++ src/Hash_to_curve.sol
@@ -0,0 +1,316 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {console} from "forge-std/Test.sol";
+
+contract Hash_to_curve {
+    uint8 HTF_L = 64;
+
+    //         function HashToG1(msg)
+    //     fieldElement0 = HashToBase(msg, 0x00, 0x01)
+    //     fieldElement1 = HashToBase(msg, 0x02, 0x03)
+    //     curveElement0 = BaseToG1(fieldElement0)
+    //     curveElement1 = BaseToG1(fieldElement1)
+    //     g1Element = ECAdd(curveElement0, curveElement1)
+    //     return g1Element
+    // end function
+    // hash_to_curve(msg)
+    // Input: msg, an arbitrary-length byte string.
+    // Output: P, a point in G.
+    // Steps:
+    // 1. u = hash_to_field(msg, 2)
+    // 2. Q0 = map_to_curve(u[0])
+    // 3. Q1 = map_to_curve(u[1])
+    // 4. R = Q0 + Q1              # Point addition
+    // 5. P = clear_cofactor(R)
+    // 6. return P
+    function hash_to_curve_g1(bytes calldata message) public {}
+
+    // hash_to_field(msg, count)
+    // Parameters:
+    // - DST, a domain separation tag (see Section 3.1).
+    // - F, a finite field of characteristic p and order q = p^m.
+    // - p, the characteristic of F (see immediately above).
+    // - m, the extension degree of F, m >= 1 (see immediately above).
+    // - L = ceil((ceil(log2(p)) + k) / 8), where k is the security
+    //   parameter of the suite (e.g., k = 128).
+    // - expand_message, a function that expands a byte string and
+    //   domain separation tag into a uniformly random byte string
+    //   (see Section 5.3).
+    // Input:
+    // - msg, a byte string containing the message to hash.
+    // - count, the number of elements of F to output.
+    // Output:
+    // - (u_0, ..., u_(count - 1)), a list of field elements.
+    // Steps:
+    // 1. len_in_bytes = count * m * L
+    // 2. uniform_bytes = expand_message(msg, DST, len_in_bytes)
+    // 3. for i in (0, ..., count - 1):
+    // 4.   for j in (0, ..., m - 1):
+    // 5.     elm_offset = L * (j + i * m)
+    // 6.     tv = substr(uniform_bytes, elm_offset, L)
+    // 7.     e_j = OS2IP(tv) mod p
+    // 8.   u_i = (e_0, ..., e_(m - 1))
+    // 9. return (u_0, ..., u_(count - 1))
+    function hash_to_field_fq2(
+        bytes calldata message,
+        uint8 count,
+        bytes memory domain
+    ) public view returns (bytes[][] memory) {
+        uint8 M = 2;
+        uint8 len_in_bytes = count * M * HTF_L; // HTF_L is 64
+        // this field_modulus as hex 4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559787
+        bytes
+            memory modulus = hex"1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab";
+        bytes
+            memory one = hex"0000000000000000000000000000000000000000000000000000000000000001";
+
+        bytes memory pseudo_random_bytes = expand_msg_xmd(
+            message,
+            len_in_bytes,
+            domain
+        );
+
+        bytes[][] memory u = new bytes[][](count);
+        for (uint i = 0; i < count; i++) {
+            bytes[] memory e = new bytes[](M);
+            for (uint j = 0; j < M; j++) {
+                uint256 offset = HTF_L * (j + i * M);
+
+                bytes memory tv = new bytes(HTF_L);
+
+                for (uint k = 0; k < HTF_L; k++) {
+                    tv[k] = pseudo_random_bytes[k + offset];
+                }
+                // console.log("tv");
+                // console.logBytes(tv);
+                // console.logBytes(modulus);
+                // console.logBytes(_modexp(tv, one, modulus));
+                e[j] = _modexp(tv, one, modulus);
+            }
+            u[i] = e;
+        }
+        return u;
+    }
+
+    // expand_message_xmd(msg, DST, len_in_bytes)
+
+    // Parameters:
+    // - H, a hash function (see requirements above).
+    // - b_in_bytes, b / 8 for b the output size of H in bits.
+    //   For example, for b = 256, b_in_bytes = 32.
+    // - s_in_bytes, the input block size of H, measured in bytes (see
+    //   discussion above). For example, for SHA-256, s_in_bytes = 64.
+
+    // Input:
+    // - msg, a byte string.
+    // - DST, a byte string of at most 255 bytes.
+    //   See below for information on using longer DSTs.
+    // - len_in_bytes, the length of the requested output in bytes,
+    //   not greater than the lesser of (255 * b_in_bytes) or 2^16-1.
+
+    // Output:
+    // - uniform_bytes, a byte string.
+
+    // Steps:
+    // 1.  ell = ceil(len_in_bytes / b_in_bytes)
+    // 2.  ABORT if ell > 255 or len_in_bytes > 65535 or len(DST) > 255
+    // 3.  DST_prime = DST || I2OSP(len(DST), 1)
+    // 4.  Z_pad = I2OSP(0, s_in_bytes)
+    // 5.  l_i_b_str = I2OSP(len_in_bytes, 2)
+    // 6.  msg_prime = Z_pad || msg || l_i_b_str || I2OSP(0, 1) || DST_prime
+    // 7.  b_0 = H(msg_prime)
+    // 8.  b_1 = H(b_0 || I2OSP(1, 1) || DST_prime)
+    // 9.  for i in (2, ..., ell):
+    // 10.    b_i = H(strxor(b_0, b_(i - 1)) || I2OSP(i, 1) || DST_prime)
+    // 11. uniform_bytes = b_1 || ... || b_ell
+    // 12. return substr(uniform_bytes, 0, len_in_bytes)
+    // len_in_bytes is supposed to be able to be bigger but for now we just use 255  to simplify the code
+    function expand_msg_xmd(
+        bytes calldata message,
+        uint8 len_in_bytes,
+        bytes memory dst
+    ) public pure returns (bytes memory) {
+        // 1.  ell = ceil(len_in_bytes / b_in_bytes)
+        // 2.  ABORT if ell > 255 or len_in_bytes > 65535 or len(DST) > 255
+        // b_in_bytes seems to be 32 for sha256
+        // ceil the division
+        uint ell = (len_in_bytes - 1) / 32 + 1;
+
+        require(ell <= 255, "len_in_bytes too large for sha256");
+        require(len_in_bytes <= 65535, "len_in_bytes too large");
+        // no length normalizing via hashing
+        require(dst.length <= 255, "dst too long");
+
+        bytes memory dst_prime = bytes.concat(dst, bytes1(uint8(dst.length)));
+
+        // 4.  Z_pad = I2OSP(0, s_in_bytes)
+        // this should be sha256 blocksize so 64 bytes
+        bytes
+            memory zpad = hex"00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+
+        // 5.  l_i_b_str = I2OSP(len_in_bytes, 2)
+        // length in byte string?
+        bytes2 l_i_b_str = bytes2(uint16(len_in_bytes));
+
+        // 6.  msg_prime = Z_pad || msg || l_i_b_str || I2OSP(0, 1) || DST_prime
+        bytes memory msg_prime = abi.encodePacked(
+            zpad,
+            message,
+            l_i_b_str,
+            hex"00",
+            dst_prime
+        );
+        // console.log("msg_prime");
+        // console.logBytes(msg_prime);
+
+        bytes32[] memory b = new bytes32[](ell + 1);
+
+        // 7.  b_0 = H(msg_prime)
+        b[0] = sha256(msg_prime);
+
+        // 8.  b_1 = H(b_0 || I2OSP(1, 1) || DST_prime)
+        b[1] = sha256(abi.encodePacked(b[0], hex"01", dst_prime));
+        // console.log("b1");
+        // console.logBytes32(b[1]);
+
+        bytes memory pseudo_random_bytes = abi.encodePacked(b[1]);
+        // console.log("pseudo_random_bytes");
+        // console.logBytes(pseudo_random_bytes);
+
+        // 9.  for i in (2, ..., ell):
+        for (uint8 i = 2; i <= ell; i++) {
+            // 10.    b_i = H(strxor(b_0, b_(i - 1)) || I2OSP(i, 1) || DST_prime)
+            bytes memory tmp = abi.encodePacked(b[0] ^ b[i - 1]);
+            tmp = abi.encodePacked(tmp, i, dst_prime);
+            b[i] = sha256(tmp);
+
+            // 11. uniform_bytes = b_1 || ... || b_ell
+            pseudo_random_bytes = abi.encodePacked(
+                pseudo_random_bytes,
+                sha256(tmp)
+            );
+        }
+        // console.log("pseudo_random_bytes");
+        // console.logBytes(pseudo_random_bytes);
+
+        // 12. return substr(uniform_bytes, 0, len_in_bytes)
+        bytes memory a = new bytes(len_in_bytes);
+        for (uint i = 0; i < len_in_bytes; i++) {
+            a[i] = pseudo_random_bytes[i];
+        }
+        return a;
+    }
+
+    // From https://github.com/firoorg/solidity-BigNumber/blob/master/src/BigNumbers.sol
+
+    /** @notice Modular Exponentiation: Takes bytes values for base, exp, mod and calls precompile for (base^exp)%^mod
+     * @dev modexp: Wrapper for built-in modexp (contract 0x5) as described here:
+     *              https://github.com/ethereum/EIPs/pull/198
+     *
+     * @param _b bytes base
+     * @param _e bytes base_inverse
+     * @param _m bytes exponent
+     * @param r bytes result.
+     */
+    function _modexp(
+        bytes memory _b,
+        bytes memory _e,
+        bytes memory _m
+    ) private view returns (bytes memory r) {
+        assembly {
+            let bl := mload(_b)
+            let el := mload(_e)
+            let ml := mload(_m)
+
+            let freemem := mload(0x40) // Free memory pointer is always stored at 0x40
+
+            mstore(freemem, bl) // arg[0] = base.length @ +0
+
+            mstore(add(freemem, 32), el) // arg[1] = exp.length @ +32
+
+            mstore(add(freemem, 64), ml) // arg[2] = mod.length @ +64
+
+            // arg[3] = base.bits @ + 96
+            // Use identity built-in (contract 0x4) as a cheap memcpy
+            let success := staticcall(
+                450,
+                0x4,
+                add(_b, 32),
+                bl,
+                add(freemem, 96),
+                bl
+            )
+
+            // arg[4] = exp.bits @ +96+base.length
+            let size := add(96, bl)
+            success := staticcall(
+                450,
+                0x4,
+                add(_e, 32),
+                el,
+                add(freemem, size),
+                el
+            )
+
+            // arg[5] = mod.bits @ +96+base.length+exp.length
+            size := add(size, el)
+            success := staticcall(
+                450,
+                0x4,
+                add(_m, 32),
+                ml,
+                add(freemem, size),
+                ml
+            )
+
+            switch success
+            case 0 {
+                invalid()
+            } //fail where we haven't enough gas to make the call
+
+            // Total size of input = 96+base.length+exp.length+mod.length
+            size := add(size, ml)
+            // Invoke contract 0x5, put return value right after mod.length, @ +96
+            success := staticcall(
+                sub(gas(), 1350),
+                0x5,
+                freemem,
+                size,
+                add(freemem, 0x60),
+                ml
+            )
+
+            switch success
+            case 0 {
+                invalid()
+            } //fail where we haven't enough gas to make the call
+
+            let length := ml
+            let msword_ptr := add(freemem, 0x60)
+
+            ///the following code removes any leading words containing all zeroes in the result.
+            for {
+
+            } eq(eq(length, 0x20), 0) {
+
+            } {
+                // for(; length!=32; length-=32)
+                switch eq(mload(msword_ptr), 0) // if(msword==0):
+                case 1 {
+                    msword_ptr := add(msword_ptr, 0x20)
+                } //     update length pointer
+                default {
+                    break
+                } // else: loop termination. non-zero word found
+                length := sub(length, 0x20)
+            }
+            r := sub(msword_ptr, 0x20)
+            mstore(r, length)
+
+            // point to the location of the return value (length, bits)
+            //assuming mod length is multiple of 32, return value is already in the right format.
+            mstore(0x40, add(add(96, freemem), ml)) //deallocate freemem pointer
+        }
+    }
+}
---
+++ test/Hash_to_curve.sol
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Hash_to_curve} from "../src/Hash_to_curve.sol";
+
+contract Hash_to_curveTest is Test {
+    Hash_to_curve public hasher;
+    bytes DST = "QUUX-V01-CS02-with-expander";
+
+    function setUp() public {
+        hasher = new Hash_to_curve();
+    }
+
+    // test cases from https://github.com/ethereum/py_ecc/blob/main/tests/bls/test_expand_message_xmd.py
+    function test_expand_msg_xmd() public view {
+        bytes memory result = hasher.expand_msg_xmd("", 0x20, DST);
+        bytes
+            memory expected = hex"f659819a6473c1835b25ea59e3d38914c98b374f0970b7e4c92181df928fca88";
+        assertEq(result, expected);
+    }
+
+    function test_expand_msg_abc_0x20() public view {
+        bytes memory result = hasher.expand_msg_xmd("abc", 0x20, DST);
+        bytes
+            memory expected = hex"1c38f7c211ef233367b2420d04798fa4698080a8901021a795a1151775fe4da7";
+        assertEq(result, expected);
+    }
+
+    function test_expand_msg_abcdef0123456789_0x20() public view {
+        bytes memory result = hasher.expand_msg_xmd(
+            "abcdef0123456789",
+            0x20,
+            DST
+        );
+        bytes
+            memory expected = hex"8f7e7b66791f0da0dbb5ec7c22ec637f79758c0a48170bfb7c4611bd304ece89";
+        assertEq(result, expected);
+    }
+
+    function test_expand_msg_q128_0x20() public view {
+        bytes memory result = hasher.expand_msg_xmd(
+            "q128_qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq",
+            0x20,
+            DST
+        );
+        bytes
+            memory expected = hex"72d5aa5ec810370d1f0013c0df2f1d65699494ee2a39f72e1716b1b964e1c642";
+        assertEq(result, expected);
+    }
+
+    function test_expand_msg_a512_0x20() public view {
+        bytes memory result = hasher.expand_msg_xmd(
+            "a512_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+            0x20,
+            DST
+        );
+        bytes
+            memory expected = hex"3b8e704fc48336aca4c2a12195b720882f2162a4b7b13a9c350db46f429b771b";
+        assertEq(result, expected);
+    }
+
+    function test_expand_msg_empty_0x80() public view {
+        bytes memory result = hasher.expand_msg_xmd("", 0x80, DST);
+        bytes
+            memory expected = hex"8bcffd1a3cae24cf9cd7ab85628fd111bb17e3739d3b53f89580d217aa79526f1708354a76a402d3569d6a9d19ef3de4d0b991e4f54b9f20dcde9b95a66824cbdf6c1a963a1913d43fd7ac443a02fc5d9d8d77e2071b86ab114a9f34150954a7531da568a1ea8c760861c0cde2005afc2c114042ee7b5848f5303f0611cf297f";
+        assertEq(result, expected);
+    }
+
+    function test_expand_msg_abc_0x80() public view {
+        bytes memory result = hasher.expand_msg_xmd("abc", 0x80, DST);
+        bytes
+            memory expected = hex"fe994ec51bdaa821598047b3121c149b364b178606d5e72bfbb713933acc29c186f316baecf7ea22212f2496ef3f785a27e84a40d8b299cec56032763eceeff4c61bd1fe65ed81decafff4a31d0198619c0aa0c6c51fca15520789925e813dcfd318b542f8799441271f4db9ee3b8092a7a2e8d5b75b73e28fb1ab6b4573c192";
+        assertEq(result, expected);
+    }
+
+    function test_expand_msg_abcdef0123456789_0x80() public view {
+        bytes memory result = hasher.expand_msg_xmd(
+            "abcdef0123456789",
+            0x80,
+            DST
+        );
+        bytes
+            memory expected = hex"c9ec7941811b1e19ce98e21db28d22259354d4d0643e301175e2f474e030d32694e9dd5520dde93f3600d8edad94e5c364903088a7228cc9eff685d7eaac50d5a5a8229d083b51de4ccc3733917f4b9535a819b445814890b7029b5de805bf62b33a4dc7e24acdf2c924e9fe50d55a6b832c8c84c7f82474b34e48c6d43867be";
+        assertEq(result, expected);
+    }
+
+    function test_expand_msg_q128_0x80() public view {
+        bytes memory result = hasher.expand_msg_xmd(
+            "q128_qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq",
+            0x80,
+            DST
+        );
+        bytes
+            memory expected = hex"48e256ddba722053ba462b2b93351fc966026e6d6db493189798181c5f3feea377b5a6f1d8368d7453faef715f9aecb078cd402cbd548c0e179c4ed1e4c7e5b048e0a39d31817b5b24f50db58bb3720fe96ba53db947842120a068816ac05c159bb5266c63658b4f000cbf87b1209a225def8ef1dca917bcda79a1e42acd8069";
+        assertEq(result, expected);
+    }
+
+    function test_expand_msg_a512_0x80() public view {
+        bytes memory result = hasher.expand_msg_xmd(
+            "a512_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+            0x80,
+            DST
+        );
+        bytes
+            memory expected = hex"396962db47f749ec3b5042ce2452b619607f27fd3939ece2746a7614fb83a1d097f554df3927b084e55de92c7871430d6b95c2a13896d8a33bc48587b1f66d21b128a1a8240d5b0c26dfe795a1a842a0807bb148b77c2ef82ed4b6c9f7fcb732e7f94466c8b51e52bf378fba044a31f5cb44583a892f5969dcd73b3fa128816e";
+        assertEq(result, expected);
+    }
+
+    function test_hash_to_field_fq2() public view {
+        bytes[][] memory result = hasher.hash_to_field_fq2("", 1, DST);
+        bytes
+            memory expected1 = hex"16da1e6feccd22e6c66989dacceb151ff125450611b39ea5ca765cda844710d51af4dc626861306a3eccf92145b5d47b";
+        bytes
+            memory expected2 = hex"11ff784ffcc1f8a96dc093531449285022df656d2bc377c29ccbf7029aac742c0fead154443b8a07062a6a8f1da0fe9d";
+        assertEq(result[0][0], expected1);
+        assertEq(result[0][1], expected2);
+    }
+}
