2153b3976315a4ba4d27953b16dd70912b7e88f9
ethyla <ethyla@disroot.org>
2024-04-08 15:58:52 +0200

    add natspec

--- src/HashToCurve.sol
+++ src/HashToCurve.sol
@@ -22,9 +22,16 @@ struct G2Point {
     bytes y_I;
 }
 
+/// @title Hash to curve for the BLS12-381 curve
+/// @author ethyla
+/// @dev Uses the eip-2537 precompiles
+/// @custom:experimental This is an experimental contract, no guarantees are made
 contract HashToCurve {
-    // Input: msg, an arbitrary-length byte string.
-    // Output: P, a point in G.
+    /// @notice Computes a point in G1 from a message
+    /// @dev Uses the eip-2537 precompiles
+    /// @param message Arbitrarylength byte string to be hashed
+    /// @param dst The domain separation tag
+    /// @return A point in G1
     function hashToCurveG1(
         bytes calldata message,
         bytes calldata dst
@@ -32,11 +39,11 @@ contract HashToCurve {
         // 1. u = hash_to_field(msg, 2)
         FieldPoint[2] memory u = hashToFieldFp(message, dst);
         // 2. Q0 = map_to_curve(u[0])
-        bytes32[4] memory q0 = mapFpToG1(u[0].u);
+        bytes32[4] memory q0 = _mapFpToG1(u[0].u);
         // 3. Q1 = map_to_curve(u[1])
-        bytes32[4] memory q1 = mapFpToG1(u[1].u);
+        bytes32[4] memory q1 = _mapFpToG1(u[1].u);
         // 4. R = Q0 + Q1              # Point addition
-        bytes32[4] memory r = addG1(q0, q1);
+        bytes32[4] memory r = _addG1(q0, q1);
         // 5. P = clear_cofactor(R)
         // Not needed as map fp to g1 already does it
         // 6. return P
@@ -47,6 +54,11 @@ contract HashToCurve {
         return p;
     }
 
+    /// @notice Computes a point in G2 from a message
+    /// @dev Uses the eip-2537 precompiles
+    /// @param message Arbitrarylength byte string to be hashed
+    /// @param dst The domain separation tag
+    /// @return A point in G2
     function hashToCurveG2(
         bytes calldata message,
         bytes calldata dst
@@ -54,11 +66,11 @@ contract HashToCurve {
         // 1. u = hash_to_field(msg, 2)
         FieldPoint2[2] memory u = hashToFieldFp2(message, dst);
         // 2. Q0 = map_to_curve(u[0])
-        bytes32[8] memory q0 = mapFp2ToG2(u[0]);
+        bytes32[8] memory q0 = _mapFp2ToG2(u[0]);
         // 3. Q1 = map_to_curve(u[1])
-        bytes32[8] memory q1 = mapFp2ToG2(u[1]);
+        bytes32[8] memory q1 = _mapFp2ToG2(u[1]);
         // 4. R = Q0 + Q1              # Point addition
-        bytes32[8] memory r = addG2(q0, q1);
+        bytes32[8] memory r = _addG2(q0, q1);
         // 5. P = clear_cofactor(R)
         // Not needed as map fp to g1 already does it
         // 6. return P
@@ -72,177 +84,45 @@ contract HashToCurve {
         return p;
     }
 
-    // adds two G1 points using the precompile
-    function addG1(
-        bytes32[4] memory point1,
-        bytes32[4] memory point2
-    ) internal view returns (bytes32[4] memory) {
-        bytes32[8] memory input;
-        input[0] = point1[0];
-        input[1] = point1[1];
-        input[2] = point1[2];
-        input[3] = point1[3];
-        input[4] = point2[0];
-        input[5] = point2[1];
-        input[6] = point2[2];
-        input[7] = point2[3];
+    /// @notice Computes a field point from a message
+    /// @dev Follows https://datatracker.ietf.org/doc/html/rfc9380#section-5.2
+    /// @param message Arbitrarylength byte string to be hashed
+    /// @param dst The domain separation tag
+    /// @return Two field points
+    function hashToFieldFp(
+        bytes calldata message,
+        bytes calldata dst
+    ) public view returns (FieldPoint[2] memory) {
+        // len_in_bytes = count * m * HTF_L
+        // so always 2 * 1 * 64 = 128
+        uint16 lenInBytes = 128;
 
-        bytes32[4] memory result;
+        bytes32[] memory pseudoRandomBytes = expandMsgXmd(
+            message,
+            dst,
+            lenInBytes
+        );
+        FieldPoint[2] memory u;
 
-        // //    ABI for G1 addition precompile
-        // // G1 addition call expects 256 bytes as an input that is interpreted as byte concatenation of two G1 points (128 bytes each). Output is an encoding of addition operation result - single G1 point (128 bytes).
-        assembly {
-            let success := staticcall(
-                100000, /// gas should be 600
-                0x0a, // address of BLS12_G1ADD
-                input, //input offset
-                256, // input size
-                result, // output offset
-                128 // output size
-            )
-            switch success
-            case 0 {
-                invalid()
-            } //fail where we haven't enough gas to make the call
-        }
+        // No loop here saves 800 gas
+        // uint8 HTF_L = 64;
+        // bytes memory tv = new bytes(64);
+        // uint256 elm_offset = 0 * 2;
+        // tv = bytes.concat(pseudo_random_bytes[0], pseudo_random_bytes[1]);
+        u[0].u = _modfield(pseudoRandomBytes[0], pseudoRandomBytes[1]);
 
-        return result;
+        // uint256 elm_offset2 = 1 * 2;
+        // tv = bytes.concat(pseudo_random_bytes[2], pseudo_random_bytes[3]);
+        u[1].u = _modfield(pseudoRandomBytes[2], pseudoRandomBytes[3]);
+
+        return u;
     }
 
-    // adds two G2 points using the precompile
-    function addG2(
-        bytes32[8] memory point1,
-        bytes32[8] memory point2
-    ) internal view returns (bytes32[8] memory) {
-        bytes32[16] memory input;
-
-        input[0] = point1[0];
-        input[1] = point1[1];
-        input[2] = point1[2];
-        input[3] = point1[3];
-        input[4] = point1[4];
-        input[5] = point1[5];
-        input[6] = point1[6];
-        input[7] = point1[7];
-
-        input[8] = point2[0];
-        input[9] = point2[1];
-        input[10] = point2[2];
-        input[11] = point2[3];
-        input[12] = point2[4];
-        input[13] = point2[5];
-        input[14] = point2[6];
-        input[15] = point2[7];
-
-        bytes32[8] memory result;
-
-        // ABI for G2 addition precompile
-        // G2 addition call expects 512 bytes as an input that is interpreted as byte concatenation of two G2 points (256 bytes each). Output is an encoding of addition operation result - single G2 point (256 bytes).
-        assembly {
-            let success := staticcall(
-                100000, /// gas should be 4500
-                0x0d, // address of BLS12_G2ADD
-                input, //input offset
-                512, // input size
-                result, // output offset
-                256 // output size
-            )
-            switch success
-            case 0 {
-                invalid()
-            } //fail where we haven't enough gas to make the call
-        }
-
-        return result;
-    }
-
-    // maps a field point to a G1 point using the precompile
-    function mapFpToG1(
-        bytes32[2] memory input
-    ) internal view returns (bytes32[4] memory) {
-        // bytes32 a;
-        // bytes32 b;
-        // assembly {
-        //     a := mload(add(fp, 0x20))
-        //     b := mload(add(fp, 0x40))
-        // }
-        // bytes32[2] memory input;
-        // input[0] = a;
-        // input[1] = b;
-
-        bytes32[4] memory result;
-
-        // ABI for mapping Fp element to G1 point precompile
-        // Field-to-curve call expects 64 bytes an an input that is interpreted as a an element of the base field. Output of this call is 128 bytes and is G1 point following respective encoding rules.
-        assembly {
-            let success := staticcall(
-                100000, /// gas should be 5500
-                0x11, // address of BLS12_MAP_FP_TO_G1
-                input, //input offset
-                64, // input size
-                result, // output offset
-                128 // output size
-            )
-            switch success
-            case 0 {
-                invalid()
-            } //fail where we haven't enough gas to make the call
-        }
-
-        return result;
-    }
-
-    // maps a field point 2 to a G2 point using the precompile
-    function mapFp2ToG2(
-        FieldPoint2 memory fp2
-    ) internal view returns (bytes32[8] memory) {
-        // bytes memory fp = bytes.concat(fp2.u, fp2.u_I);
-
-        // bytes32 a;
-        // bytes32 b;
-        // bytes32 c;
-        // bytes32 d;
-        // assembly {
-        //     a := mload(add(fp, 0x20))
-        //     b := mload(add(fp, 0x40))
-        //     c := mload(add(fp, 0x60))
-        //     d := mload(add(fp, 0x80))
-        // }
-
-        bytes32[4] memory input;
-        input[0] = fp2.u[0];
-        input[1] = fp2.u[1];
-        input[2] = fp2.u_I[0];
-        input[3] = fp2.u_I[1];
-
-        bytes32[8] memory result;
-
-        // ABI for mapping Fp2 element to G2 point precompile
-        // Field-to-curve call expects 128 bytes an an input that is interpreted as a an element of the quadratic extension field. Output of this call is 256 bytes and is G2 point following respective encoding rules.
-        assembly {
-            let success := staticcall(
-                200000, /// gas should be 110000
-                0x12, // address of BLS12_MAP_FP2_TO_G2
-                input, //input offset
-                128, // input size
-                result, // output offset
-                256 // output size
-            )
-            switch success
-            case 0 {
-                invalid()
-            } //fail where we haven't enough gas to make the call
-        }
-
-        return result;
-    }
-
-    // https://datatracker.ietf.org/doc/html/rfc9380#section-5.2
-    // Input:
-    // - msg, a byte string containing the message to hash.
-    // - count, the number of elements of F to output.
-    // count is always 2 for curve to hash usage
-    // - DST, a domain separation tag (see Section 3.1).
+    /// @notice Computes a field point from a message
+    /// @dev Follows https://datatracker.ietf.org/doc/html/rfc9380#section-5.2
+    /// @param message Arbitrarylength byte string to be hashed
+    /// @param dst The domain separation tag
+    /// @return Two field points
     function hashToFieldFp2(
         bytes calldata message,
         bytes calldata dst
@@ -275,47 +155,13 @@ contract HashToCurve {
         return u;
     }
 
-    // Input:
-    // - msg, a byte string containing the message to hash.
-    // - count, the number of elements of F to output.
-    // count is always 2 for hash to curve usage
-    // - DST, a domain separation tag (see Section 3.1).
-    function hashToFieldFp(
-        bytes calldata message,
-        bytes calldata dst
-    ) public view returns (FieldPoint[2] memory) {
-        // len_in_bytes = count * m * HTF_L
-        // so always 2 * 1 * 64 = 128
-        uint16 lenInBytes = 128;
-
-        bytes32[] memory pseudoRandomBytes = expandMsgXmd(
-            message,
-            dst,
-            lenInBytes
-        );
-        FieldPoint[2] memory u;
-
-        // No loop here saves 800 gas
-        // uint8 HTF_L = 64;
-        // bytes memory tv = new bytes(64);
-        // uint256 elm_offset = 0 * 2;
-        // tv = bytes.concat(pseudo_random_bytes[0], pseudo_random_bytes[1]);
-        u[0].u = _modfield(pseudoRandomBytes[0], pseudoRandomBytes[1]);
-
-        // uint256 elm_offset2 = 1 * 2;
-        // tv = bytes.concat(pseudo_random_bytes[2], pseudo_random_bytes[3]);
-        u[1].u = _modfield(pseudoRandomBytes[2], pseudoRandomBytes[3]);
-
-        return u;
-    }
-
-    // https://datatracker.ietf.org/doc/html/rfc9380#section-5.2
-    // Input:
-    // - msg, a byte string.
-    // - DST, a byte string of at most 255 bytes.
-    // - len_in_bytes, the length of the requested output in bytes,
-    //   not greater than the lesser of (255 * b_in_bytes) or 2^16-1.
-    // returns bytes32[] because len_in_bytes is always a multiple of 32 in our case even 128
+    /// @notice Computes a field point from a message
+    /// @dev Follows https://datatracker.ietf.org/doc/html/rfc9380#section-5.3
+    /// @dev bytes32[] because len_in_bytes is always a multiple of 32 in our case even 128
+    /// @param message Arbitrarylength byte string to be hashed
+    /// @param dst The domain separation tag of at most 32 bytes (should be 255)
+    /// @param lenInBytes The length of the requested output in bytes
+    /// @return A field point
     function expandMsgXmd(
         bytes calldata message,
         bytes calldata dst,
@@ -374,17 +220,8 @@ contract HashToCurve {
         return b;
     }
 
-    // Originaly from https://github.com/firoorg/solidity-BigNumber/blob/master/src/BigNumbers.sol
-
-    /** @notice Modular Exponentiation: Takes bytes values for base, exp, mod and calls precompile for (base^exp)%^mod
-     * @dev modexp: Wrapper for built-in modexp (contract 0x5) as described here:
-     *              https://github.com/ethereum/EIPs/pull/198
-     *
-     * @param _b1 bytes32 base first 32 bytes
-     * @param _b2 bytes32 base last 32 bytes
-     * @param r bytes result.
-     */
     // passing two bytes32 instead of bytes memory saves approx 700 gas per call
+    // Computes the mod against the bls12-381 field modulus
     function _modfield(
         bytes32 _b1,
         bytes32 _b2
@@ -444,5 +281,170 @@ contract HashToCurve {
             //adjust freemem pointer
             mstore(0x40, add(add(freemem, 0x60), ml))
         }
+    }
+
+    // adds two G1 points using the precompile
+    function _addG1(
+        bytes32[4] memory point1,
+        bytes32[4] memory point2
+    ) internal view returns (bytes32[4] memory) {
+        bytes32[8] memory input;
+        input[0] = point1[0];
+        input[1] = point1[1];
+        input[2] = point1[2];
+        input[3] = point1[3];
+        input[4] = point2[0];
+        input[5] = point2[1];
+        input[6] = point2[2];
+        input[7] = point2[3];
+
+        bytes32[4] memory result;
+
+        // //    ABI for G1 addition precompile
+        // // G1 addition call expects 256 bytes as an input that is interpreted as byte concatenation of two G1 points (128 bytes each). Output is an encoding of addition operation result - single G1 point (128 bytes).
+        assembly {
+            let success := staticcall(
+                100000, /// gas should be 600
+                0x0a, // address of BLS12_G1ADD
+                input, //input offset
+                256, // input size
+                result, // output offset
+                128 // output size
+            )
+            switch success
+            case 0 {
+                invalid()
+            } //fail where we haven't enough gas to make the call
+        }
+
+        return result;
+    }
+
+    // adds two G2 points using the precompile
+    function _addG2(
+        bytes32[8] memory point1,
+        bytes32[8] memory point2
+    ) internal view returns (bytes32[8] memory) {
+        bytes32[16] memory input;
+
+        input[0] = point1[0];
+        input[1] = point1[1];
+        input[2] = point1[2];
+        input[3] = point1[3];
+        input[4] = point1[4];
+        input[5] = point1[5];
+        input[6] = point1[6];
+        input[7] = point1[7];
+
+        input[8] = point2[0];
+        input[9] = point2[1];
+        input[10] = point2[2];
+        input[11] = point2[3];
+        input[12] = point2[4];
+        input[13] = point2[5];
+        input[14] = point2[6];
+        input[15] = point2[7];
+
+        bytes32[8] memory result;
+
+        // ABI for G2 addition precompile
+        // G2 addition call expects 512 bytes as an input that is interpreted as byte concatenation of two G2 points (256 bytes each). Output is an encoding of addition operation result - single G2 point (256 bytes).
+        assembly {
+            let success := staticcall(
+                100000, /// gas should be 4500
+                0x0d, // address of BLS12_G2ADD
+                input, //input offset
+                512, // input size
+                result, // output offset
+                256 // output size
+            )
+            switch success
+            case 0 {
+                invalid()
+            } //fail where we haven't enough gas to make the call
+        }
+
+        return result;
+    }
+
+    // maps a field point to a G1 point using the precompile
+    function _mapFpToG1(
+        bytes32[2] memory input
+    ) internal view returns (bytes32[4] memory) {
+        // bytes32 a;
+        // bytes32 b;
+        // assembly {
+        //     a := mload(add(fp, 0x20))
+        //     b := mload(add(fp, 0x40))
+        // }
+        // bytes32[2] memory input;
+        // input[0] = a;
+        // input[1] = b;
+
+        bytes32[4] memory result;
+
+        // ABI for mapping Fp element to G1 point precompile
+        // Field-to-curve call expects 64 bytes an an input that is interpreted as a an element of the base field. Output of this call is 128 bytes and is G1 point following respective encoding rules.
+        assembly {
+            let success := staticcall(
+                100000, /// gas should be 5500
+                0x11, // address of BLS12_MAP_FP_TO_G1
+                input, //input offset
+                64, // input size
+                result, // output offset
+                128 // output size
+            )
+            switch success
+            case 0 {
+                invalid()
+            } //fail where we haven't enough gas to make the call
+        }
+
+        return result;
+    }
+
+    // maps a field point 2 to a G2 point using the precompile
+    function _mapFp2ToG2(
+        FieldPoint2 memory fp2
+    ) internal view returns (bytes32[8] memory) {
+        // bytes memory fp = bytes.concat(fp2.u, fp2.u_I);
+
+        // bytes32 a;
+        // bytes32 b;
+        // bytes32 c;
+        // bytes32 d;
+        // assembly {
+        //     a := mload(add(fp, 0x20))
+        //     b := mload(add(fp, 0x40))
+        //     c := mload(add(fp, 0x60))
+        //     d := mload(add(fp, 0x80))
+        // }
+
+        bytes32[4] memory input;
+        input[0] = fp2.u[0];
+        input[1] = fp2.u[1];
+        input[2] = fp2.u_I[0];
+        input[3] = fp2.u_I[1];
+
+        bytes32[8] memory result;
+
+        // ABI for mapping Fp2 element to G2 point precompile
+        // Field-to-curve call expects 128 bytes an an input that is interpreted as a an element of the quadratic extension field. Output of this call is 256 bytes and is G2 point following respective encoding rules.
+        assembly {
+            let success := staticcall(
+                200000, /// gas should be 110000
+                0x12, // address of BLS12_MAP_FP2_TO_G2
+                input, //input offset
+                128, // input size
+                result, // output offset
+                256 // output size
+            )
+            switch success
+            case 0 {
+                invalid()
+            } //fail where we haven't enough gas to make the call
+        }
+
+        return result;
     }
 }
